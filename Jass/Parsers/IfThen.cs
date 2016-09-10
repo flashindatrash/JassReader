59cb73570be0605de8db021144d47253c0afef32
iSeeYou87 <merzavec87@inbox.ru>
2016-09-10 21:53:03 +0300

    add suported elseif

--- Jass/Parsers/IfThen.cs
+++ Jass/Parsers/IfThen.cs
@@ -3,12 +3,17 @@ using System.Text.RegularExpressions;
 
 namespace Jass
 {
-	public class IfThen : IParser
+	public class IfThen : JassLine, IParser, ITabBefore, ITabAfter
 	{
 		public const string Pattern = @"^(?<type>if|elseif)\s+(?<predicate>" + Predicate.Pattern + @")\s+then\r?$";
 
 		private string type;
 		private readonly Predicate predicate = new Predicate();
+
+		public bool isElse
+		{
+			get { return type == "elseif"; }
+		}
 
 		public void Parse(string text)
 		{
@@ -22,6 +27,15 @@ namespace Jass
 			return (type == "elseif" ? "} else if " : "if ") + "(" + predicate + ") {";
 		}
 
+		public int tabBefore
+		{
+			get { return isElse ? 1 : 0; }
+		}
+
+		public int tabAfter
+		{
+			get { return 1; }
+		}
 	}
 }
 
