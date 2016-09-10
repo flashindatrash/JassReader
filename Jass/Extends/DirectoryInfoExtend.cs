d770bc1cd36a3cf7419dea2bfeabac835b34f6bd
iSeeYou87 <merzavec87@inbox.ru>
2016-09-10 21:59:09 +0300

    Extend class DirectoryInfo

---
+++ Jass/Extends/DirectoryInfoExtend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Jass
+{
+	public static class DirectoryInfoExtend
+	{
+		public static void Clear(this DirectoryInfo directory)
+		{
+			foreach (FileInfo file in directory.GetFiles()) file.Delete();
+			foreach (DirectoryInfo subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+		}
+	}
+}
+
