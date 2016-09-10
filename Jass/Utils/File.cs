c73bf328feba60e1c919914d367eb7c367a723ca
iSeeYou87 <merzavec87@inbox.ru>
2016-09-10 21:58:49 +0300

    File Utils

---
+++ Jass/Utils/File.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jass
+{
+	public static class File
+	{
+
+		private static Dictionary<string, string> _templates = new Dictionary<string, string>();
+
+		public static string Get(string path)
+		{
+			if (!_templates.ContainsKey(path))
+			{
+				try
+				{
+					_templates[path] = System.IO.File.ReadAllText(path);
+				}
+				catch (Exception ex)
+				{
+					Log.Add(ex.Message, ConsoleColor.Red);
+					return "";
+				}
+			}
+			return _templates[path];
+		}
+
+	}
+}
+
